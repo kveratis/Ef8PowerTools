00cc8d56b911149297935da3b5c9c6c34c127638
David Perish <david.perish@caremetx.com>
2024-04-25 20:18:40 -0400

    Add project files.

---
+++ Ef8PowerTools.WebApi/Controllers/ProgramsController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ef8PowerTools.Data.Models;
+
+namespace Ef8PowerTools.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgramsController : ControllerBase
+    {
+        private readonly SummaContext _context;
+
+        public ProgramsController(SummaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Programs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Data.Models.Program>>> GetPrograms()
+        {
+            return await _context.Programs.ToListAsync();
+        }
+
+        // GET: api/Programs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Data.Models.Program>> GetProgram(int id)
+        {
+            var program = await _context.Programs.FindAsync(id);
+
+            if (program == null)
+            {
+                return NotFound();
+            }
+
+            return program;
+        }
+
+        // PUT: api/Programs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProgram(int id, Data.Models.Program program)
+        {
+            if (id != program.ProgramId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(program).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProgramExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Programs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Data.Models.Program>> PostProgram(Data.Models.Program program)
+        {
+            _context.Programs.Add(program);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProgramExists(program.ProgramId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProgram", new { id = program.ProgramId }, program);
+        }
+
+        // DELETE: api/Programs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProgram(int id)
+        {
+            var program = await _context.Programs.FindAsync(id);
+            if (program == null)
+            {
+                return NotFound();
+            }
+
+            _context.Programs.Remove(program);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProgramExists(int id)
+        {
+            return _context.Programs.Any(e => e.ProgramId == id);
+        }
+    }
+}
