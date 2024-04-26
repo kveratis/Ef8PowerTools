b364d2b06fc73fe36a27303d0d04f26038d3a350
David Perish <david.perish@caremetx.com>
2024-04-25 20:40:39 -0400

    Migrate to new db 'SummaEf'

--- Ef8PowerTools.Data/SummaContext.cs
+++ Ef8PowerTools.Data/SummaContext.cs
@@ -1612,7 +1612,7 @@ public partial class SummaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=Summa;Persist Security Info=True;User ID=sa;Password=IamInPlainTextNotSecure##2021;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=SummaEf;Persist Security Info=True;User ID=sa;Password=IamInPlainTextNotSecure##2021;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
